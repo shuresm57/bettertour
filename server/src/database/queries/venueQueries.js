4b6a13ea7a272f4b27089f1334f34dc21f386801
shuresm57 <vsorensen31@gmail.com>
2026-05-26 23:49:18 +0200

    removed the unused findByEmail

--- server/src/database/queries/venueQueries.js
+++ server/src/database/queries/venueQueries.js
@@ -1,14 +1,5 @@
 import db from '../connection.js';
 
-export function findVenueByEmail (email) {
-  return db.prepare(`
-    SELECT venue.*, venue_user.user_id
-    FROM venue
-    JOIN venue_user ON venue.venue_id = venue_user.venue_id
-    WHERE venue.contact_email = ?
-    `).get(email);
-}
-
 export function updateVenue (venueId, data) {
   const { name, address, bio, contactEmail } = data;
   return db.prepare(`
